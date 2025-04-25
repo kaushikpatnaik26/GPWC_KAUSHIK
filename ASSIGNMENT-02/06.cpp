8b40910d909f41fa1a3cb914dfe2eb1d2c59746b
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-04-25 18:07:34 +0530

    create 06.cpp

---
+++ ASSIGNMENT-02/06.cpp
@@ -0,0 +1,20 @@
+Texture texture;
+texture.loadfilefrom("Soa.png");
+Sprite sprite;
+sprite.setTexture(texture);
+
+RenderWindow windoe(VideoMode(960,540), "Soa Logo");
+while(window.isOpen())
+{
+    Event event;
+    while(window.pollEvent(event))
+    {
+        if(event.type = event::closed())
+        {
+            window.close();
+        }
+        window.clear();
+        window.draw(sprite);
+        window.display();   
+    }
+}
