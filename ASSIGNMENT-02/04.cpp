9c201b3a441168f13f2fa51bad0ccad976c85522
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2025-04-29 06:20:58 +0530

    update 04.cpp

--- ASSIGNMENT-02/04.cpp
+++ ASSIGNMENT-02/04.cpp
@@ -1,1 +1,4 @@
-RenderWindow window (VideoMode(960,540)"MY RENDER WINDOW");
+// RenderWindow window (VideoMode(960,540), "MY RENDER WINDOW", Style::Fulscreen);
+// or
+// VideoMode v(960,540);
+// RenderWindow window(v,"Timberman Game" Style::Fullscreen);
